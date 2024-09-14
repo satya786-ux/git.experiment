28612ad3be5bb5ba8b0279df81e015133666f08f
satya786-ux <harisatyacoc@gmali.com>
2024-09-14 22:33:29 +0530

    programs are added

---
+++ unicode telugu.c
@@ -0,0 +1,14 @@
+//print unicode in telugu leltters
+#include <stdio.h>
+
+int main() {
+  printf("telugu letters:\n");
+  printf(" \u0c05 \u0c06 \u0c07 \u0c08 \u0c09 \u0c0A \u0c0B \u0c60 \u0c0E \u0c0F \u0c10 \u0c12 \u0c13 \u0c14 \u0c05\u0c02 \u0c05\u0c03 \n
+   \u0c15 \u0C16 \u0c17 \u0c18 \u0C19 \n 
+   \u0c1A \u0c1B \u0c1C \u0c1D \u0c1E \n 
+   \u0c1F \u0c20 \u0c21 \u0c22 \u0c23 \n 
+   \u0c24 \u0c25 \u0c26 \u0c27 \u0c28 \n 
+   \u0c2A \u0c2B \u0c2C \u0c2D \u0c2E \n 
+   \u0c2F \u0c30 \u0c32 \u0c35 \u0c36 \u0c37 \u0c38 \u0c39 \u0c33 \u0c31");
+ return 0;
+}
